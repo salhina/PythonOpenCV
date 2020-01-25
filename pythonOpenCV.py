eef0149f60740a802f211469278ee49176a3d39a
Nabil SALHI <52040368+salhina@users.noreply.github.com>
2020-01-25 10:43:29 +0400

    Update pythonOpenCV.py

--- pythonOpenCV.py
+++ pythonOpenCV.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 
 import numpy as np
-
 import cv2
 
 img = cv2.imread('pythonOpenCV.png')
 
 cv2.imshow('My photo', img)
-
 cv2.waitKey(0)
-
 cv2.destroyAllWindows()
-
-#cv2.destroyAllWindows()
